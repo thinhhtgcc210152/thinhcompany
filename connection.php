15f3637d58168a92ac4ad00f8a3d457099d2b881
thinhhtgcc210152 <thinhhtgcc210152@fpt.edu.vn>
2023-06-20 18:28:14 +0700

    asd

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'technology_accessories')
+    $conn = mysqli_connect('frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'm7il8elv0jh7jqus', 'xxzmq2b7fe51fg9r', 'tfku7jk8wfnn4vsd')
             or die("Cannot connect database".mysqli_connect_error());
 ?>
